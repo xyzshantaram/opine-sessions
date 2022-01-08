d9be6631710ac4f95c8e05052b0ce7154ee23e12
Siddharth Singh <simsid66@gmail.com>
2022-01-09 01:53:05 +0530

    fix export

--- mod.ts
+++ mod.ts
@@ -1,4 +1,6 @@
 import { init, destroy, getClient } from "./src/sessions.ts";
 
 export type { Store } from './src/sessions.ts';
+
+export { init, destroy, getClient };
 export default { init, destroy, getClient };
