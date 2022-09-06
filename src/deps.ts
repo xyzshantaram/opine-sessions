0f3a360bfa11d742795cbdd7183b41e3e90d98a1
Siddharth Singh <simsid66@gmail.com>
2022-09-06 22:53:38 +0530

    update opine vers

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export type { Opine, OpineRequest, OpineResponse, NextFunction, CookieOptions } from "https://deno.land/x/opine@2.1.1/mod.ts";
+export type { Opine, OpineRequest, OpineResponse, NextFunction, CookieOptions } from "https://deno.land/x/opine@2.3.3/mod.ts";
 export { getCookies } from "https://deno.land/std@0.116.0/http/cookie.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.2.0/mod.ts";
 export type { Row } from "https://deno.land/x/sqlite@v3.2.0/mod.ts";
